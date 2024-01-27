5b3d52d17171f4fab54c7b0d1484034a49404431
Thinh Tran <thinhphuctran@gmail.com>
2024-01-27 08:44:17 +0700

    add

---
+++ after/plugin/nvim-r.lua
@@ -0,0 +1,8 @@
+vim.g.voom_ft_modes = {
+    markdown = 'pandoc',
+    rmd = 'pandoc',
+    quarto = 'pandoc',
+    rnoweb = 'latex'
+}
+
+vim.g.R_assign = 0
---
+++ lua/thinh/init.lua
@@ -0,0 +1,6 @@
+require("thinh.set")
+require("thinh.remap")
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
